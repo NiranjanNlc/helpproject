2e2cff03a080b3a61b241eea4e4836132e20168c
niranjannlc <niranjannlc10@gmail.com>
2020-05-19 14:49:32 +0545

    configured for wordpress

---
+++ build/precache-manifest.6adcc4b8ada9bc5bdbdfb137038a2f68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa838e64f06a5956ec90f36eb927e136",
+    "url": "/help/react/index.html"
+  },
+  {
+    "revision": "637cffdcd839ae9e0970",
+    "url": "/help/react/static/css/main.7d0b5fc0.chunk.css"
+  },
+  {
+    "revision": "3f0dbbf92f29dfdb1bc5",
+    "url": "/help/react/static/js/2.5ec4c9c2.chunk.js"
+  },
+  {
+    "revision": "3a08a7c8f8e6231522abe29a929e6912",
+    "url": "/help/react/static/js/2.5ec4c9c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "637cffdcd839ae9e0970",
+    "url": "/help/react/static/js/main.3a5be1b5.chunk.js"
+  },
+  {
+    "revision": "c84d6c6ecdcf2cb4db82",
+    "url": "/help/react/static/js/runtime-main.0ed83990.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/help/react/static/media/logo.5d5d9eef.svg"
+  }
+]);
